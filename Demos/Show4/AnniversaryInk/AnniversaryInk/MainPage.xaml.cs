8c1410b8aafbb77a05036d040329b6892be35667
Mike Taulty <mtaulty@microsoft.com>
2016-05-13 15:36:54 +0100

    added basic anniversary ink demo

---
+++ Demos/Show4/AnniversaryInk/AnniversaryInk/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace AnniversaryInk
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+      this.Loaded += OnLoaded;
+    }
+    void OnLoaded(object sender, RoutedEventArgs e)
+    {
+      this.inkCanvas.InkPresenter.InputDeviceTypes =
+        CoreInputDeviceTypes.Mouse |
+        CoreInputDeviceTypes.Pen |
+        CoreInputDeviceTypes.Touch;
+    }
+  }
+}
